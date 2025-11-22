4fd9007958dfad6a5ef62b11c39d88639ad8b8a4
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-11-22 21:23:29 +1100

    add board class description

---
+++ board.rb
@@ -0,0 +1,10 @@
+class Board
+  # board class to:
+  # show dashes for each letter in word
+  # receive a guess and check against word
+  # if guess in word, show letter on board
+  # if guess not in word, show with guessed letters - add an X for attempts
+  # if already guessed, "try again"
+  # check for complete word
+  # check for guesses left
+end
