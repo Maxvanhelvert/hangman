6f4eb81518c46dc6a8a2754211f8b38b10965a3f
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-11-23 14:50:07 +1100

    functional basic hangman game

--- board.rb
+++ board.rb
@@ -7,4 +7,97 @@ class Board
   # if already guessed, "try again"
   # check for complete word
   # check for guesses left
+
+  def initialize(answer)
+    @answer = answer
+    @board = []
+    set_board
+    @life_num = 10
+    @lives = []
+    set_lives
+    @wrong_guesses = []
+    @guess = ''
+  end
+
+  def guess(guess)
+    @guess = guess
+  end
+
+  def set_board
+    @answer.each_char do |char|
+      @board.push('-')
+    end
+    @board
+  end
+
+  def show_board
+    @board.each do |char|
+      print char
+      print ' '
+    end
+    puts
+  end
+
+  def show_lives
+    puts 'Lives: '
+    @lives.each do |life|
+      print life
+      print ' '
+    end
+    puts
+  end
+
+  def show_wrong_guesses
+    puts 'Wrong guesses: '
+    @wrong_guesses.each do |guess|
+      print guess
+      print ' '
+    end
+    puts
+  end
+
+  def set_lives
+    count = 0
+    while count < @life_num do
+      @lives.push('●')
+      count += 1
+    end
+    @lives
+  end
+
+  def lose_life
+    @lives[@life_num - 1] = 'X'
+
+    @life_num -= 1
+
+    @life_num
+  end
+
+  def wrong_guess
+    @wrong_guesses.push(@guess)
+    lose_life
+  end
+
+  def correct_guess
+    temp_ans = @answer.split('')
+    temp_ans.each_with_index do |char, i|
+      @board[i] = @guess if char == @guess
+    end
+  end
+
+  def check_guess
+    if @answer.include?(@guess)
+      correct_guess
+    else
+      wrong_guess
+    end
+  end
+
+  def finished?
+    game = false
+    game = 'win' unless @board.include?('-') 
+    game = 'lose' if @life_num <= 0
+    game
+  end
+
 end
--- main.rb
+++ main.rb
@@ -6,11 +6,47 @@
 # if word guess finish game
 # if all guesses used and word not guessed, finish game.
 require './dictionary'
+require './board'
 
 def play_game
   dictionary = Dictionary.new
   word = dictionary.get_word
-  p word
+  current_board = Board.new(word)
+
+  current_board.show_lives
+  current_board.show_board
+  current_board.show_wrong_guesses
+
+  while !current_board.finished?
+    play_round(current_board)
+  end
+
+  result = current_board.finished?
+  puts
+  puts 'Game Finished'
+  puts "You #{result}"
+  puts
+  puts "The word was #{word}"
+
+end
+
+def play_round(board)
+  puts
+  puts 'Type your guess: '
+  guess = gets.chomp.downcase
+  puts
+
+  while !guess.match?(/\A[a-z]\Z/)
+    puts "Try again: "
+    guess = gets.chomp.downcase
+  end
+
+  board.guess(guess)
+  board.check_guess
+
+  board.show_lives
+  board.show_board
+  board.show_wrong_guesses
 end
 
 play_game
